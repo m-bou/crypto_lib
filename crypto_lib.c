27985a7876c071eac253f4662cd9716290dfd75d
Mehdi B <pro.mehdi.bouchet@gmail.com>
2023-02-17 13:16:32 +0100

    Translate english

--- crypto_lib.c
+++ crypto_lib.c
@@ -10,10 +10,11 @@
 
 void process_karatsuba(){
     int n, m;
-    printf("Calcul de n*m : ");
+    printf("Let's calculate of n * m\n");
     
-    printf("Choisir n : "); scanf("%d", &n);
-    printf("Choisir m : "); scanf("%d", &m);
+    printf("Choose n : "); scanf("%d", &n);
+    printf("Choose m : "); scanf("%d", &m);
+    printf("\n");
 
     if(DEBUG_ON) printf("n= %d ; m= %d\n\n", n, m);
 
@@ -25,35 +26,34 @@ void process_karatsuba(){
 void process_shanks(){
     int a, p;
 
-    printf("Calcul de sqrt(a) mod p : \n");
+    printf("Let's calculate x where a = x^2 mod p : \n");
     
-    printf("Choisir a : "); scanf("%d", &a);
-    printf("Choisir p : "); scanf("%d", &p);
+    printf("Choose a : "); scanf("%d", &a);
+    printf("Choose p : "); scanf("%d", &p);
+    printf("\n");
 
     if(DEBUG_ON) printf("a= %d ; p= %d\n", a, p);
 
     int x= shanks(a, p);
 
-    printf("Racine carrée de %d mod p = %d\n", a, x);
+    printf("Square root of %d mod p = %d\n", a, x);
 }
 
 void process_primality(){
     long N;
-    printf("Test primalité de n\n");
-    printf("Choisir n : "); scanf("%ld", &N);
+    printf("Primality test of n\n");
+    printf("Choose n : "); scanf("%ld", &N);
+    printf("\n");
 
-    if( N <= 2 || N >= MAX_LONG_SIZE)
-        return;
+    if(N >= MAX_LONG_SIZE) return;
+    if( N  < 0 ){ printf("n must be greater or equal to 0\n"); return; }
+    if( N <= 1 ){ printf("1 is not prime\n"); return; }
+    if( N <= 2 ){ printf("%ld is prime\n", N); return; }
+    if( !(N&1) ){ printf("N is not prime\n"); return; }
 
-    if( !(N&1) ){
-        printf("Entier pair != 2 donc non premier\n\n");
-        return;
-    }
-
-    srand( time( NULL ) );
-    printf("Test de primalité de Fermat : %d\n",  primality_fermat(N));
-    printf("Test de primalité de Solovay-Strassen : %d\n",  primality_solovay_strassen(N, SS_TEST_ROUND));
-    printf("Test de primalité de Miller-Rabin : %d\n",  primality_miller_rabin(N, SS_TEST_ROUND) );
+    printf("Primality test of Fermat : %d\n",  primality_fermat(N));
+    printf("Primality test of Solovay-Strassen : %d\n",  primality_solovay_strassen(N, SS_TEST_ROUND));
+    printf("Primality test of Miller-Rabin : %d\n",  primality_miller_rabin(N, SS_TEST_ROUND) );
 }
 
 void process_factor(){
@@ -72,43 +72,49 @@ void process_factor(){
     mpz_init(p);
     mpz_init(q);
 
-    printf("Choisir N : ");
+    printf("Let's factorize N\n");
+    printf("Choose N : ");
+    printf("\n");
+
     if( !scanf(" %49[0-9 ]", N_str) )
         return;
-
-    printf("Good\n");
 
     mpz_init_set_str(N, N_str, 10);
 
     if( mpz_probab_prime_p(N, 24) ){
-        gmp_printf("%Zd est premier.\n", N);
+        gmp_printf("%Zd is prime.\n", N);
         return;
     }
 
     facto_pollard(p, N);
     mpz_cdiv_q(q, N, p);
 
-    gmp_printf("Factorisation Rho-Pollard sur %Zd = p * q avec\n", N);
+    gmp_printf("Factorization of Rho-Pollard on %Zd = p * q with\n", N);
     gmp_printf("p: %Zd\n", p);
     gmp_printf("q: %Zd\n", q);
 
 }
 
 int main(int argc, char** argv){
+    srand( time( NULL ) );
+    
     int c;
-    printf("Bienvenue dans ma super bibliothèque cryptographique !\n");
+    printf("Welcome on my super cryptographic lib !\n");
     
     while(1){
-        printf("\n*********** Choissir un outil ***********\n\n");
+        printf("\n************ Choose a tool **************\n");
+        printf("*\t\t\t\t\t*\n");
+        printf("* 1 - Multiplication Karatsuba \t\t*\n");
+        printf("* 2 - Root Shanks-Tonelli    \t\t*\n");
+        printf("* 3 - Primality Test        \t\t*\n");
+        printf("* 4 - Factorization Rho-Pollard\t\t*\n");
         printf("* 0 - Exit \t\t\t\t*\n");
-        printf("* 1 - Multiplication karatsuba \t\t*\n");
-        printf("* 2 - Racine Shanks-Tonelli    \t\t*\n");
-        printf("* 3 - Test de Primalité        \t\t*\n");
-        printf("* 4 - Factorisation Rho-Pollard\t\t*\n");
-        printf("\t\t\t\t\t*\n");
+        printf("*\t\t\t\t\t*\n");
         printf("*****************************************\n");
 
-        printf("Votre choix : "); scanf("%d", &c);
+        printf("Your choice : "); scanf("%d", &c);
+        fflush(stdin);
+
         printf("\n");
 
         switch(c){
@@ -119,6 +125,17 @@ int main(int argc, char** argv){
             case 0:default: printf("Au revoir !\n"); return 0;
 
         }
+
+        printf("\n*****************************************\n");
+        printf("*\t\t\t\t\t*\n");
+        printf("*\tPress [ENTER] to Continue\t*\n"); 
+        printf("*\t\t\t\t\t*\n");
+        printf("*****************************************\n");
+
+        while(getchar()!='\n'); // option TWO to clean stdin
+        // fflush(stdin);
+        getchar(); // wait for ENTER
+
     }
     
 }
